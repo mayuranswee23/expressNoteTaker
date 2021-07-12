1ffe1caf919a7cd5243ccf89ed4000986d68f30d
Mayuran <mayuranswee23@gmail.com>
2021-07-11 23:22:47 -0400

    added ports and routes to server.js

--- Develop/server.js
+++ Develop/server.js
@@ -3,4 +3,16 @@ const express = require('express');
 const PORT = process.env.PORT || 3001;
 const app = express ();
 
-const apiRoutes = require('../routes/apiRoutes')
+const apiRoutes = require('../routes/apiRoutes');
+const htmlRoutes = require('../routes/htmlRoutes');
+
+app.use(express.urlencoded({extended:true }));
+app.use(express.json());
+app.use(express.static('public'));
+
+app.use('/api', apiRoutes);
+app.use('/', htmlRoutes);
+
+app.listen(PORt, () => {
+    console.log(`API serer is now on PORT ${PORT}`);
+});
